b0264ff377e024522a9a8dcf1477633f396a9e3b
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-03 13:12:26 +0200

    using splice, push, shift

--- Modern_JavaScript_Bootcamp/Arrays/Notes/notes.js
+++ Modern_JavaScript_Bootcamp/Arrays/Notes/notes.js
@@ -1,4 +1,21 @@
 const notes = ['Note 1', 'Note 2', 'Note 3'];
 
+// // remove the last item of the array - the removed item is provided as a returned value
+// console.log(notes.pop());
+
+// // adding intem in the end of the array
+// notes.push('My new note');
+
+// // remove the first item of the array - the removed item is provided as a returned value
+// console.log(notes.shift());
+
+// // adding item on the first place of the array
+// notes.unshift('My first note');
+
+// notes.splice(1, 0, 'This is the new second item');
+// notes.splice(1, 1, 'This is the new second item');
+
+notes[2] = 'This is now the new note 3';
+
 console.log(notes.length);
-console.log(notes[notes.length - 1]);
+console.log(notes);
--- Modern_JavaScript_Bootcamp/Arrays/ToDo/todo.js
+++ Modern_JavaScript_Bootcamp/Arrays/ToDo/todo.js
@@ -3,12 +3,25 @@
 // Print the first and second to last items -> Todo: walk the dog
 
 const todos = ['Walkt the dog', 'Go to the store', 'Pick up laundry', 'Learn something new', 'Go to run'];
+
 console.log(`You have ${todos.length} ToDos.`);
 console.log('-------------------------------');
-// for (let i = 0; i < todos.length; i++) {
-//     console.log(`Todo ${i + 1}: ${todos[i]}`);
-// }
-console.log(`Todo: ${todos[0]}`);
-console.log(`Todo: ${todos[todos.length - 2]}`);
+// console.log(`Todo: ${todos[0]}`);
+// console.log(`Todo: ${todos[todos.length - 2]}`);
+
+// Delete the 3rd item
+// Add a new item onto the end
+// Remove the first item from the list
+
+todos.splice(2, 1);
+todos.push('Go to the Gym');
+todos.shift();
+
+// console.log(todos);
 
 
+
+
+for (let i = 0; i < todos.length; i++) {
+    console.log(`Todo ${i + 1}: ${todos[i]}`);
+}

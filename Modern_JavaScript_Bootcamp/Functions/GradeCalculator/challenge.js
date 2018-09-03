0bbaf4d19f230b974c61e1bad7bd3b9a762cdfc0
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-03 11:40:46 +0200

    const

---
+++ Modern_JavaScript_Bootcamp/Functions/GradeCalculator/challenge.js
@@ -0,0 +1,23 @@
+// students score, total possible score
+// 15/20 -> You got a C (75%)!
+//A
+
+const gradeCalc = function (studentScore, maxScore) {
+    const total = Math.round((studentScore / maxScore) * 100);
+    let grade;
+    if (total < 90 && total >= 80) {
+        grade = 'B';
+    } else if (total < 80 && total >= 70) {
+        grade = 'C';
+    } else if (total < 70 && total >= 60) {
+        grade = 'D';
+    } else if (total < 60) {
+        grade = 'F';
+    } else {
+        grade = 'A';
+    }
+    return `You got a ${grade} (${total}%)!`;
+}
+
+const result = gradeCalc(9, 20);
+console.log(result);
---
+++ Modern_JavaScript_Bootcamp/Objects/Constants/const.js
@@ -0,0 +1,15 @@
+const isRaining = true;
+const person = {
+    age: 27
+}
+
+// valid - we just changing one of the objects properties
+person.age = 28;
+
+// invalid
+//person = {};
+
+// isRaining = false;
+
+console.log(isRaining);
+console.log(person);

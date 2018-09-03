d4bd6654b1bc3d20ac97aff255e5ee929755f748
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-03 14:01:46 +0200

    forEach function

--- Modern_JavaScript_Bootcamp/Arrays/ToDo/todo.js
+++ Modern_JavaScript_Bootcamp/Arrays/ToDo/todo.js
@@ -4,8 +4,6 @@
 
 const todos = ['Walkt the dog', 'Go to the store', 'Pick up laundry', 'Learn something new', 'Go to run'];
 
-console.log(`You have ${todos.length} ToDos.`);
-console.log('-------------------------------');
 // console.log(`Todo: ${todos[0]}`);
 // console.log(`Todo: ${todos[todos.length - 2]}`);
 
@@ -18,10 +16,11 @@ todos.push('Go to the Gym');
 todos.shift();
 
 // console.log(todos);
-
-
-
-
-for (let i = 0; i < todos.length; i++) {
-    console.log(`Todo ${i + 1}: ${todos[i]}`);
-}
+// for (let i = 0; i < todos.length; i++) {
+//     console.log(`Todo ${i + 1}: ${todos[i]}`);
+// }
+console.log(`You have ${todos.length} ToDos.`);
+console.log('-------------------------------');
+todos.forEach(function(item, index) {
+    console.log(`Todo ${index + 1}: ${item}`);
+});

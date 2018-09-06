0eba557b12d318d3ef9a644db7360ab7e63b270d
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-06 15:14:56 +0200

    'filtering the todos using input

--- Modern_JavaScript_Bootcamp/TodoApp/js/todo-app.js
+++ Modern_JavaScript_Bootcamp/TodoApp/js/todo-app.js
@@ -1,3 +1,4 @@
+// array of objects for ToDo
 const todos = [{
     text: 'Walk the dog',
     completed: true
@@ -15,35 +16,63 @@ const todos = [{
     completed: false
 }];
 
-// You have 2 todos left (p)
-// Add a p for each todo above (use text value)
-const todosLeft = todos.filter(function (todo) {
-    return !todo.completed;
-})
+// object of filters
+const filters = {
+    searchText: ''
+}
 
+// how many todos needs to be completed
+const todosLeft = function (todos) {
+    let toComplete = todos.filter(function (todo) {
+        return !todo.completed;
+    });
+    return toComplete.length;
+}
+
+// function for creating the elements
 const createElement = function (text, element) {
     const newParagraph = document.createElement(element);
     newParagraph.textContent = text;
-    document.querySelector('body').appendChild(newParagraph);
+    document.querySelector('#todos-div').appendChild(newParagraph);
 };
 
-const allTodos = function (todos) {
+// list all todos and put them into page
+const listAllTodos = function (todos) {
     todos.forEach(function (todo, index) {
         createElement(`${index + 1}. ${todo.text} - Completed: ${todo.completed}`, 'p');
     });
 };
+
+// todos - filtered based on the input and rendered into page
+const renderedTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+    });
+    document.querySelector('#todos-div').innerHTML = '';
+    // creating h2 - with number of not completed tasks
+    createElement(`You have ${todosLeft(filteredTodos)} todos left.`, 'h2');
+    // listed all new filtered todos
+    listAllTodos(filteredTodos);
+}
+
+// initail rendering - to be displayed something
+renderedTodos(todos, filters);
 
 // Listen for new todo creation
 document.querySelector('#add-todo').addEventListener('click', function(e){
     console.log('Add new todo!');
 })
 
+// Listen for input for new todo
 document.querySelector('#new-todo').addEventListener('input', function (e) {
     console.log(e.target.value);
+
 })
 
-createElement(`You have ${todosLeft.length} todos left.`, 'h2');
-allTodos(todos);
-
+// Listen for input for filter
+document.querySelector('#search-text').addEventListener('input', function (e) {
+    filters.searchText = e.target.value;
+    renderedTodos(todos, filters);
+})
 
 

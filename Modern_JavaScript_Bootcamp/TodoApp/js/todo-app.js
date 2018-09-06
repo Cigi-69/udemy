5e485fb1c472ba96a407be39b114ffb2f2bd6172
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-06 09:33:59 +0200

    Notes and Todo app using DOM

---
+++ Modern_JavaScript_Bootcamp/TodoApp/js/todo-app.js
@@ -0,0 +1,39 @@
+const todos = [{
+    text: 'Walk the dog',
+    completed: true
+}, {
+    text: 'Go to the store',
+    completed: false
+}, {
+    text: 'Pick up laundry',
+    completed: true
+}, {
+    text: 'Learn something new',
+    completed: true
+}, {
+    text: 'Go to run',
+    completed: false
+}];
+
+// You have 2 todos left (p)
+// Add a p for each todo above (use text value)
+const todosLeft = todos.filter(function (todo) {
+    return !todo.completed;
+})
+
+const createElement = function (text, element) {
+    const newParagraph = document.createElement(element);
+    newParagraph.textContent = text;
+    document.querySelector('body').appendChild(newParagraph);
+};
+
+const allTodos = function (todos) {
+    todos.forEach(function (todo, index) {
+        createElement(`${index + 1}. ${todo.text} - Completed: ${todo.completed}`, 'p');
+    });
+};
+
+createElement(`You have ${todosLeft.length} todos left.`, 'h2');
+allTodos(todos);
+
+

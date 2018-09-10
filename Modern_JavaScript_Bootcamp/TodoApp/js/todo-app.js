e95197715c89e4116aa5ead25d4b479ec4c95f4f
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-10 10:29:34 +0200

    Refactoring the code, challenge

--- Modern_JavaScript_Bootcamp/TodoApp/js/todo-app.js
+++ Modern_JavaScript_Bootcamp/TodoApp/js/todo-app.js
@@ -1,62 +1,10 @@
 // array of objects for ToDo
-let todos = [];
+const todos = getSavedTodos();
 
 // object of filters
 const filters = {
     searchText: '',
     checkgoxActivated: false
-}
-
-// read data from local storage
-const todosJSON = localStorage.getItem('todos');
-// if data exist - parse them into todos
-if (todosJSON !== null) {
-    todos = JSON.parse(todosJSON);
-}
-
-// function for creating new element - ELEMENT, TEXT, WHERE TO APPEND
-const createNewElement = function (element, text, appendTo) {
-    const newElement = document.createElement(element);
-    newElement.textContent = text;
-    document.querySelector(appendTo).appendChild(newElement);
-}
-
-// function for listing all todos
-const listAllTodos = function (todos) {
-    notCompletedAmount(todos);
-        todos.forEach(function (todo, index) {
-            createNewElement('p', `${index + 1}. ${todo.text} - completed: ${todo.completed}`, '#todos-div');
-        });
-}
-
-// filtering the todo - which are not completed yet
-const notCompletedAmount = function (todos) {
-    const notDone = todos.filter(function (todo) {
-        return !todo.completed;
-    });
-    createNewElement('h2', `You have ${notDone.length} todos to do.`, '#todos-div');
-}
-
-// filtering the todos
-const filterTodos = function (todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
-        // if there is a match - return true
-        const searchMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
-        // checkbox not-checked -> always true -> all todos || otherwise -> always flase -> todos with todo.completed === false;
-        const hideCompletedMatch = !filters.checkgoxActivated || !todo.completed;
-        return searchMatch && hideCompletedMatch;
-    });
-    document.querySelector('#todos-div').innerHTML = '';
-    listAllTodos(filteredTodos);
-}
-
-// function for creating new todo - pushing todo object into original array of objects
-const createNewTodo = function (todos, text) {
-    if (text.length > 0) {
-        todos.push({ text: text, completed: false });
-        // updating local sorage with new data
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }
 }
 
 // Listening for the input for searching
@@ -77,12 +25,10 @@ document.querySelector('#todo-form').addEventListener('submit', function (e) {
 // Listening for the checkbox
 document.querySelector('#hide-completed').addEventListener('change', function (e) {
     filters.checkgoxActivated = e.target.checked;
-    // document.querySelector('#todos-div').innerHTML = '';
     filterTodos(todos, filters);
 })
 
 listAllTodos(todos);
-// console.log(notCompleted(todos));
 
 
 
---
+++ Modern_JavaScript_Bootcamp/TodoApp/js/todo-functions.js
@@ -0,0 +1,58 @@
+// Read existing todos from localStorage
+const getSavedTodos = function () {
+    const todosJSON = localStorage.getItem('todos');
+    if (todosJSON !== null) {
+        return JSON.parse(todosJSON);
+    } else {
+        return [];
+    }
+}
+
+// Updating local sorage with new data
+const saveTodos = function (todos) {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+
+// Function for listing all todos
+const listAllTodos = function (todos) {
+    notCompletedAmount(todos);
+    todos.forEach(function (todo, index) {
+        createNewElement('p', `${index + 1}. ${todo.text} - completed: ${todo.completed}`, '#todos-div');
+    });
+}
+
+// Function for creating new element - ELEMENT, TEXT, WHERE TO APPEND
+const createNewElement = function (element, text, appendTo) {
+    const newElement = document.createElement(element);
+    newElement.textContent = text;
+    document.querySelector(appendTo).appendChild(newElement);
+}
+
+// Function for creating new todo - pushing todo object into original array of objects
+const createNewTodo = function (todos, text) {
+    if (text.length > 0) {
+        todos.push({ text: text, completed: false });
+        saveTodos(todos);
+    }
+}
+
+// Filtering the todo - which are not completed yet
+const notCompletedAmount = function (todos) {
+    const notDone = todos.filter(function (todo) {
+        return !todo.completed;
+    });
+    createNewElement('h2', `You have ${notDone.length} todos to do.`, '#todos-div');
+}
+
+// Filtering the todos
+const filterTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        // if there is a match - return true
+        const searchMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
+        // checkbox not-checked -> always true -> all todos || otherwise -> always flase -> todos with todo.completed === false;
+        const hideCompletedMatch = !filters.checkgoxActivated || !todo.completed;
+        return searchMatch && hideCompletedMatch;
+    });
+    document.querySelector('#todos-div').innerHTML = '';
+    listAllTodos(filteredTodos);
+}

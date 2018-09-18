f580040acd05f777eabc45ebc040ac41e4362dc0
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-18 09:49:07 +0200

    Hangman.prototype.getPuzzle

--- Modern_JavaScript_Bootcamp/HangMan/hangman.js
+++ Modern_JavaScript_Bootcamp/HangMan/hangman.js
@@ -1,11 +1,31 @@
 const Hangman = function (word, attempts) {
-    this.word = word;
+    this.word = word.toLowerCase().split('');
+    this.guessedLetters = ['a','n','y','c','e','t'];
     this.attempts = attempts;
 };
 
-const game1 = new Hangman('Cat', 3);
-const game2 = new Hangman('Dog', 3 );
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = '';
 
-console.log(game1);
-console.log(game2);
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter;
+        } else {
+            puzzle += '*';
+        }
+    });
+
+    return puzzle;
+}
+
+const game1 = new Hangman('Cat', 2);
+const game2 = new Hangman('New York', 4 );
+
+console.log(game1.getPuzzle());
+console.log(game2.getPuzzle());
+
+
+
+// console.log(game1);
+// console.log(game2);
 

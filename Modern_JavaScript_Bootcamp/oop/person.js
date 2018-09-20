8d45b894963afd563efd32bebd0e970ffe752fc2
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-20 10:17:15 +0200

    Using subclasses

--- Modern_JavaScript_Bootcamp/oop/person.js
+++ Modern_JavaScript_Bootcamp/oop/person.js
@@ -1,4 +1,6 @@
 // Prototypal Inheritance
+// myPerson --> Person.prototype --> Object.prototype --> null
+
 
 class Person {
     constructor(firstName, lastName, age, likes = []) {
@@ -21,9 +23,43 @@ class Person {
     }
 }
 
-const me = new Person('John', 'Doe', 29, ['Teaching', 'Biking']);
-me.setName('Alexis Turner')
-console.log(me.getBio());
+class Employee extends Person {
+    constructor(firstName, lastName, age, likes, position) {
+        super(firstName, lastName, age, likes);
+        this.position = position;
+    }
+    getBio() {
+        return `${this.firstName} ${this.lastName} is a ${this.position}`;
+    }
+    getYeardsLeft() {
+        return 65 - this.age;
+    }
+}
 
-const person2 = new Person('Clancey', 'Turner', 51);
-console.log(person2.getBio());
+class Student extends Person {
+    constructor(firstName, lastName, age, likes, grade) {
+        super(firstName, lastName, age, likes);
+        this.grade = grade;
+    }
+    getBio() {
+        const status = this.grade > 69 ? 'passing' : 'failing';
+         return `${this.firstName} ${this.lastName} is ${status} the test.`
+    }
+    updateGrade(points) {
+        this.grade += points;
+    }
+}
+
+// const me = new Employee('John', 'Doe', 29, ['Teaching', 'Biking'], 'Programmer');
+// me.setName('Alexis Turner')
+// console.log(me.getBio());
+// console.log(me.getYeardsLeft());
+// const person2 = new Person('Clancey', 'Turner', 51);
+// console.log(person2.getBio());
+
+const student = new Student('John', 'Doe', 25, ['programming', 'soccer'], 90);
+console.log(student.getBio());
+student.updateGrade(-45);
+console.log(student.getBio());
+
+

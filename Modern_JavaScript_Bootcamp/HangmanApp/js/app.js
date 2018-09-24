6b3e5461076eaef0250f3349f9428833139365a4
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-24 16:20:49 +0200

    Adding http request as well as callback function for getting the data

--- Modern_JavaScript_Bootcamp/HangmanApp/js/app.js
+++ Modern_JavaScript_Bootcamp/HangmanApp/js/app.js
@@ -1,3 +1,7 @@
+// HTTP (Hypertext Transfer Ptrotocol)
+// Request - What do we want to do
+// Responsee - What was actually done
+
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
 
@@ -11,3 +15,31 @@ window.addEventListener('keypress', function (e) {
     puzzleEl.textContent = game1.puzzle;
     guessesEl.textContent = game1.statusMessage;
 });
+
+getPuzzle((error, puzzle) => {
+    if (error) {
+      console.log(`Error: ${error}`);
+    } else {
+        console.log(puzzle);
+    }
+});
+
+
+
+// Making an HTTP request
+
+
+// const countryRequest = new XMLHttpRequest();
+// const countryCode = "SK";
+// countryRequest.addEventListener('readystatechange', (e) => {
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText);
+//         const country = data.find((country) => country.alpha2Code === countryCode);
+//         console.log(country.name);
+//     } else if (e.target.readyState === 4) {
+//         console.log('Unable to fetch data.');
+
+//     }
+// });
+// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
+// countryRequest.send();

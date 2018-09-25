ef1d31d6f0cb8c8ea9111bed04d04775c31ee8c2
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-25 10:01:58 +0200

    Callback function + challenge

--- Modern_JavaScript_Bootcamp/HangmanApp/js/app.js
+++ Modern_JavaScript_Bootcamp/HangmanApp/js/app.js
@@ -16,7 +16,7 @@ window.addEventListener('keypress', function (e) {
     guessesEl.textContent = game1.statusMessage;
 });
 
-getPuzzle((error, puzzle) => {
+getPuzzle('2', (error, puzzle) => {
     if (error) {
       console.log(`Error: ${error}`);
     } else {
@@ -24,25 +24,10 @@ getPuzzle((error, puzzle) => {
     }
 });
 
-console.log("do something");
-
-
-
-
-// Making an HTTP request
-
-
-// const countryRequest = new XMLHttpRequest();
-// const countryCode = "SK";
-// countryRequest.addEventListener('readystatechange', (e) => {
-//     if (e.target.readyState === 4 && e.target.status === 200) {
-//         const data = JSON.parse(e.target.responseText);
-//         const country = data.find((country) => country.alpha2Code === countryCode);
-//         console.log(country.name);
-//     } else if (e.target.readyState === 4) {
-//         console.log('Unable to fetch data.');
-
-//     }
-// });
-// countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-// countryRequest.send();
+getCountryDetail('Sk', (error, country) => {
+    if (error) {
+        console.log(`Error: ${error}`);
+    } else {
+        console.log(country.name);
+    }
+});

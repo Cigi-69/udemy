fa281401a52c0735df1e4ce679dfd78c6ac6fdf6
--global <26696326+Cigi-69@users.noreply.github.com>
2018-09-26 14:47:32 +0200

    Fetch Challenge

--- Modern_JavaScript_Bootcamp/HangmanApp/js/app.js
+++ Modern_JavaScript_Bootcamp/HangmanApp/js/app.js
@@ -22,21 +22,8 @@ getPuzzle('2').then((puzzle) => {
     console.log(`Error: ${error}`);
 });
 
-
-// getCountryDetail('SK').then((country) => {
-//     console.log(country.name);
-// }, (error) => {
-//     console.log(`Error: ${error}`);
-// });
-
-// fetch('http://puzzle.mead.io/puzzle', {}).then((response) => {
-//     if (response.status === 200) {
-//         return response.json();
-//     } else {
-//         throw new Error('Unable to fetch the puzzle');
-//     }
-// }).then((data) => {
-//     console.log(data.puzzle);
-// }).catch((error) => {
-//     console.log(error);
-// });
+getCountryDetail('SK').then((country) => {
+    console.log(country.name);
+}).catch((error) => {
+    console.log(`Error: ${error}`);
+});
--- Modern_JavaScript_Bootcamp/HangmanApp/js/requests.js
+++ Modern_JavaScript_Bootcamp/HangmanApp/js/requests.js
@@ -10,19 +10,14 @@ const getPuzzle = (wordCount) => {
     })
 }
 
-const getCountryDetail = (countryCode) => new Promise((resolve, reject) => {
-    const countryRequest = new XMLHttpRequest();
-
-    countryRequest.addEventListener('readystatechange', (e) => {
-        if (e.target.readyState === 4 && e.target.status === 200) {
-            const data = JSON.parse(e.target.responseText);
-            const country = data.find((country) => country.alpha2Code === countryCode.toUpperCase());
-            resolve(country);
-        } else if (e.target.readyState === 4) {
-            reject('An error occured.');
+const getCountryDetail = (countryCode) => {
+    return fetch('http://restcountries.eu/rest/v2/all').then((response) => {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            throw new Error('Unable to fetch data.');
         }
+    }). then((data) => {
+        return data.find((country) => country.alpha2Code === countryCode.toUpperCase());
     });
-
-    countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all');
-    countryRequest.send();
-});
+};

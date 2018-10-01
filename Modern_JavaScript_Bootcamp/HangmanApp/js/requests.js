17509b2fee597c08f497bb7b1ba36c7abb5b1c4d
--global <26696326+Cigi-69@users.noreply.github.com>
2018-10-01 09:49:35 +0200

    Refactoring the code - await Promise.all

--- Modern_JavaScript_Bootcamp/HangmanApp/js/requests.js
+++ Modern_JavaScript_Bootcamp/HangmanApp/js/requests.js
@@ -9,32 +9,17 @@ const getPuzzle = async (wordCount) => {
 }
 
 const getCurrentCountry = async () => {
-    const responseLocation = await fetch('https://ipinfo.io/json?token=34c1bbef294ab2');
-    const responseCountry = await fetch('http://restcountries.eu/rest/v2/all');
+    const [responseLocation, responseCountry] = await Promise.all([
+        fetch('https://ipinfo.io/json?token=34c1bbef294ab2'),
+        fetch('http://restcountries.eu/rest/v2/all')
+    ]);
     if (responseLocation.status === 200 && responseCountry.status === 200) {
-        const location = await responseLocation.json();
-        const countryData = await responseCountry.json();
-        return countryData.find((country) => country.alpha2Code === location.country.toUpperCase());
+        const [location, countryData] = await Promise.all([
+            responseLocation.json(),
+            responseCountry.json()
+        ]);
+        return countryData.find((country) => countra.alpha2Code === location.country.toUpperCase());
     } else {
         throw new Error('Unable to fetch data.');
     }
 };
-
-// const getCountryDetail = async (countryCode) => {
-//     const response = await fetch('http://restcountries.eu/rest/v2/all');
-//     if (response.status === 200) {
-//         const data = await response.json();
-//         return data.find((country) => country.alpha2Code === countryCode.toUpperCase());
-//     } else {
-//         throw new Error('Unable to fetch data.');
-//     }
-// };
-
-// const getLocation = async () => {
-//     const response = await fetch('https://ipinfo.io/json?token=34c1bbef294ab2');
-//     if (response.status === 200) {
-//         return await response.json();
-//     } else {
-//         throw new Error('Unable to fetch data.');
-//     }
-// };

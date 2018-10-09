aed347da61d0a98f11c3804c154f2b65d51d5b31
--global <26696326+Cigi-69@users.noreply.github.com>
2018-10-09 11:06:00 +0200

    Webpack - import/export

--- Modern_JavaScript_Bootcamp/boilerplate/src/index.js
+++ Modern_JavaScript_Bootcamp/boilerplate/src/index.js
@@ -1,11 +1,6 @@
-const name = 'Mike'
+import { add, name } from "./utilities";
+import { scream } from "./scream";
+console.log('index.js')
+console.log(add(32, 1))
 console.log(name)
-
-class Hangman {
-    myMethod() {
-        return 'testing'
-    }
-}
-const hangman = new Hangman()
-console.log(hangman.myMethod());
-
+console.log(scream(name))

373e487655328f8e1882badf14d7e92e8a22470e
--global <26696326+Cigi-69@users.noreply.github.com>
2018-10-10 16:27:22 +0200

    node_modules into gitIgnorlist

---
+++ Modern_JavaScript_Bootcamp/HangmanBWP/src/hangman.js
@@ -0,0 +1,54 @@
+class Hangman {
+    constructor(word, attempts) {
+        this.word = word.toLowerCase().split('');
+        this.guessedLetters = [];
+        this.attempts = attempts;
+        this.status = 'playing';
+    }
+    setStatus() {
+        const finished = this.word.every((letter) => this.guessedLetters.includes(letter) || letter === ' ');
+
+        if (this.attempts === 0) {
+            this.status = 'failed';
+        } else if (finished) {
+            this.status = 'finished';
+        } else {
+            this.status = 'playing';
+        }
+        return this.status;
+    }
+    get statusMessage() {
+        if (this.status === 'playing') {
+            return `Guesses left: ${this.attempts}`;
+        } else if (this.status === 'failed') {
+            return `Nice try! The word was "${this.word.join('')}".`;
+        } else {
+            return 'Great work! You guessed the word.';
+        }
+    }
+    get puzzle() {
+        let puzzle = '';
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ' ') {
+                puzzle += letter;
+            } else {
+                puzzle += '*';
+            }
+        });
+        return puzzle;
+    }
+    makeGuess(guess) {
+        guess = guess.toLowerCase();
+
+        if (this.status !== 'playing') {
+            return
+        }
+
+        if (!this.guessedLetters.includes(guess)) {
+            this.word.includes(guess) ? this.guessedLetters.push(guess) : this.attempts--;
+            this.setStatus();
+        }
+    }
+}
+
+export {Hangman as default}

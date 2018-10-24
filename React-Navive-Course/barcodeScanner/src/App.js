5d6a2808816cf617d93afa5f77af33e60282dd1c
--global <26696326+Cigi-69@users.noreply.github.com>
2018-10-24 22:44:29 +0200

    Working version of the Barcode Scenner App

--- React-Navive-Course/barcodeScanner/src/App.js
+++ React-Navive-Course/barcodeScanner/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, PermissionsAndroid } from 'react-native';
+import { View, PermissionsAndroid, Alert, Vibration } from 'react-native';
 import BarcodeScanner, {
        Exception, FocusMode,
        CameraFillMode, FlashMode, BarcodeType
 } from 'react-native-barcode-scanner-google';
+import { PlaySound } from 'react-native-play-sound';
+import RNExitApp from 'react-native-exit-app';
 import { Header, Card, CardSection, Input, Button } from './components/common';
 
 class App extends Component {
-  state = { cameraOn: false, orderBcValue: '', itemBcValue: '', type: '' };
+  state = { cameraOn: false,
+    orderBcValue: '',
+    itemBcValue: '',
+    type: '',
+    orderStyle: 'black',
+    itemStyle: 'black'
+  };
+
    componentWillMount() {
      this.requestCameraPermission();
    }
@@ -19,21 +28,35 @@ class App extends Component {
   async requestCameraPermission() {
     try {
       const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: 'Cool Photo App Camera Permission',
-          message: 'Cool Photo App needs access to your camera ' +
-          'so you can take awesome pictures.'
-        }
+        PermissionsAndroid.PERMISSIONS.CAMERA
       );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
         console.log('You can use the camera');
       } else {
-        console.log('Camera permission denied');
+        Alert.alert('Barcode Scanner app cannot work correctly without' +
+        'camera permission, therefore will be closed now.');
+        RNExitApp.exitApp();
       }
     } catch (err) {
       console.warn(err);
     }
+  }
+
+  checkBC(dataToCheck, str) {
+    let result = true;
+    let testingValue = /^\d{5}$/g;
+    if (str === 'order') {
+      testingValue = /^[a-zA-Z0-9]{8}$/g;
+    }
+    if (!testingValue.test(dataToCheck)) {
+      result = false;
+    }
+    return result;
+  }
+
+  feedback() {
+    PlaySound('warning');
+    Vibration.vibrate(1000);
   }
 
   renderContent() {
@@ -46,18 +69,34 @@ class App extends Component {
               onBarcodeRead={({ data }) => {
                   if (this.state.type === 'order') {
                     this.setState({ orderBcValue: data });
+                    const result = this.checkBC(data, this.state.type);
+                    if (result) {
+                      this.setState({ orderStyle: 'green' });
+                    } else {
+                      this.setState({ orderStyle: 'red' });
+                      this.feedback();
+                    }
                   } else {
                     this.setState({ itemBcValue: data });
+                    const result = this.checkBC(data, this.state.type);
+                    if (result) {
+                      this.setState({ itemStyle: 'green' });
+                    } else {
+                      this.setState({ itemStyle: 'red' });
+                      this.feedback();
+                    }
                   }
                   this.setState({ cameraOn: false });
               }}
               onException={exceptionKey => {
                   switch (exceptionKey) {
                       case Exception.NO_PLAY_SERVICES:
+                        Alert.alert('For the correct behavior of the Barcode ' +
+                        'Scanner app you have to update Google Play Store first.');
                       break;
                       case Exception.LOW_STORAGE:
-                      break;
-                      case Exception.NOT_OPERATIONAL:
+                        Alert.alert('No enough space. For the correct behavior ' +
+                        'of the Barcode Scanner app you have to have enough space first.');
                       break;
                       default: break;
                   }
@@ -77,6 +116,12 @@ class App extends Component {
         <Card>
           <CardSection>
             <Input
+              style={{
+                color: this.state.orderStyle,
+                height: 40,
+                borderWidth: 2,
+                paddingLeft: 10
+              }}
               onFocus={() => {
                 this.setState({ type: 'order' });
                 this.setState({ cameraOn: !this.cameraOn });
@@ -88,6 +133,12 @@ class App extends Component {
           </CardSection>
           <CardSection>
             <Input
+            style={{
+              color: this.state.itemStyle,
+              height: 40,
+              borderWidth: 2,
+              paddingLeft: 10
+            }}
               onFocus={() => {
                 this.setState({ type: 'item' });
                 this.setState({ cameraOn: !this.cameraOn });
@@ -99,10 +150,7 @@ class App extends Component {
           </CardSection>
         </Card>
       );
-      default:
-        return (
-          <Text>Nic</Text>
-        );
+      default: break;
     }
   }
 
--- React-Navive-Course/barcodeScanner/src/components/common/Input.js
+++ React-Navive-Course/barcodeScanner/src/components/common/Input.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, Text, TextInput } from 'react-native';
 
-const Input = ({ label, value, placeholder, onFocus }) => {
-  const { containerStyle, labelStyle, inputStyle } = styles;
+const Input = ({ label, value, placeholder, onFocus, style }) => {
+  const { containerStyle, labelStyle } = styles;
   return (
     <View style={containerStyle}>
       <Text style={labelStyle}>{label}</Text>
       <TextInput
         onFocus={onFocus}
-        style={inputStyle}
+        style={style}
         value={value}
         placeholder={placeholder}
       />
@@ -27,11 +27,6 @@ const styles = {
     fontSize: 16,
     paddingLeft: 4,
     width: 100
-  },
-  inputStyle: {
-    height: 40,
-    borderWidth: 2,
-    paddingLeft: 10
   }
 };
 
